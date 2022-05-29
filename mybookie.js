929b316826ed86bb1846d850365d150098dc0034
chhatraman shrestha <cms111000111@gmail.com>
2022-05-30 00:44:28 +0545

    my bookie whole added

--- mybookie.js
+++ mybookie.js
@@ -16,25 +16,23 @@ class PuppeteerService {
 
     }
 
-    initiate(isLinkCrawlTest) {
-        return new Promise(async (resolve, reject) => {
-
-            let El;
-            this.pageOptions = {
-                waitUntil: 'networkidle2',
-                // timeout: 30000000
-            };
-            this.waitForFunction = 'document.querySelector("body")';
-            puppeteerExtra.use(pluginStealth());
-            // const browser = await puppeteerExtra.launch({ headless: true });
-            this.browser = await puppeteerExtra.launch({ headless: false });
-            this.page = await this.browser.newPage();
-            console.log("page inititated");
-            
-            this.isLinkCrawlTest = isLinkCrawlTest;
-            var link = this.generateLink();
-            return await this.crawl(link);
-        });
+    async initiate(isLinkCrawlTest) {
+        let El;
+        this.pageOptions = {
+            waitUntil: 'networkidle2',
+            // timeout: 30000000
+        };
+        this.waitForFunction = 'document.querySelector("body")';
+        puppeteerExtra.use(pluginStealth());
+        // const browser = await puppeteerExtra.launch({ headless: true });
+        this.browser = await puppeteerExtra.launch({ headless: false });
+        this.page = await this.browser.newPage();
+        console.log("page inititated");
+        
+        this.isLinkCrawlTest = isLinkCrawlTest;
+        var link = this.generateLink();
+        return await this.crawl(link);
+        
 
 
     }
@@ -62,57 +60,61 @@ class PuppeteerService {
             await this.page.waitForSelector('.game-lines');
             let urls = await this.page.evaluate(()=>{
                 const datas=[];
-                const mainList=  document.querySelectorAll('.game-lines');
+                const mainList=  document.querySelectorAll('.game-lines .line-default .game-line');
                 mainList.forEach(localData => {
-                    
-                    const metaDatas=localData.querySelector('.game-line .event-wrapper');
-                    const id=localData.querySelectorAll('.game-line .game-line__visitor-line button')[0].getAttribute('data-gameid');
-                    const visitor={
-                        teamname:localData.querySelectorAll('.game-line .game-line__visitor-line button')[0].getAttribute('data-team'),
-                        spread:{
-                            spread:localData.querySelector('.game-line .game-line__visitor-line button[data-wager-type="sp"]').getAttribute('data-spread'),
-                            odd:localData.querySelector('.game-line .game-line__visitor-line button[data-wager-type="sp"]').getAttribute('data-odds')
-                        },
-                        moneyline:{
-                            spread:localData.querySelector('.game-line .game-line__visitor-line button[data-wager-type="ml"]').getAttribute('data-spread'),
-                            odd:localData.querySelector('.game-line .game-line__visitor-line button[data-wager-type="ml"]').getAttribute('data-odds')
-                        },
-                        total:{
-                            spread:localData.querySelector('.game-line .game-line__visitor-line button[data-wager-type="to"]').getAttribute('data-spread'),
-                            odd:localData.querySelector('.game-line .game-line__visitor-line button[data-wager-type="to"]').getAttribute('data-odds')
-                        }
-                    };
-                    const home={
-                        teamname:localData.querySelectorAll('.game-line .game-line__home-line button')[0].getAttribute('data-team'),
-                        spread:{
-                            spread:localData.querySelector('.game-line .game-line__home-line button[data-wager-type="sp"]').getAttribute('data-spread'),
-                            odd:localData.querySelector('.game-line .game-line__home-line button[data-wager-type="sp"]').getAttribute('data-odds')
-                        },
-                        moneyline:{
-                            spread:localData.querySelector('.game-line .game-line__home-line button[data-wager-type="ml"]').getAttribute('data-spread'),
-                            odd:localData.querySelector('.game-line .game-line__home-line button[data-wager-type="ml"]').getAttribute('data-odds')
-                        },
-                        total:{
-                            spread:localData.querySelector('.game-line .game-line__home-line button[data-wager-type="to"]').getAttribute('data-spread'),
-                            odd:localData.querySelector('.game-line .game-line__home-line button[data-wager-type="to"]').getAttribute('data-odds')
-                        }
-                    };
-                    console.log(visitor);
-                    datas.push({
-                        start:metaDatas.querySelector('meta[itemprop="startDate"]').getAttribute('content'),
-                        end:metaDatas.querySelector('meta[itemprop="endDate"]').getAttribute('content'),
-                        name:metaDatas.querySelector('meta[itemprop="performer"]').getAttribute('content'),
-                        visitor:visitor,
-                        home:home,
-                        id:id,
-                        url:'https://www.mybookie.ag/sportsbook/nba/?prop='+id
-                    })
+                    try {
+                        const metaDatas=localData.querySelector('.event-wrapper');
+                        const id=localData.querySelectorAll('.game-line__visitor-line button')[0].getAttribute('data-gameid');
+                        const visitor={
+                            teamname:localData.querySelectorAll('.game-line__visitor-line button')[0].getAttribute('data-team'),
+                            spread:{
+                                spread:localData.querySelector('.game-line__visitor-line button[data-wager-type="sp"]').getAttribute('data-spread'),
+                                odd:localData.querySelector('.game-line__visitor-line button[data-wager-type="sp"]').getAttribute('data-odds')
+                            },
+                            moneyline:{
+                                spread:localData.querySelector('.game-line__visitor-line button[data-wager-type="ml"]').getAttribute('data-spread'),
+                                odd:localData.querySelector('.game-line__visitor-line button[data-wager-type="ml"]').getAttribute('data-odds')
+                            },
+                            total:{
+                                spread:localData.querySelector('.game-line__visitor-line button[data-wager-type="to"]').getAttribute('data-spread'),
+                                odd:localData.querySelector('.game-line__visitor-line button[data-wager-type="to"]').getAttribute('data-odds')
+                            }
+                        };
+                        const home={
+                            teamname:localData.querySelectorAll('.game-line__home-line button')[0].getAttribute('data-team'),
+                            spread:{
+                                spread:localData.querySelector('.game-line__home-line button[data-wager-type="sp"]').getAttribute('data-spread'),
+                                odd:localData.querySelector('.game-line__home-line button[data-wager-type="sp"]').getAttribute('data-odds')
+                            },
+                            moneyline:{
+                                spread:localData.querySelector('.game-line__home-line button[data-wager-type="ml"]').getAttribute('data-spread'),
+                                odd:localData.querySelector('.game-line__home-line button[data-wager-type="ml"]').getAttribute('data-odds')
+                            },
+                            total:{
+                                spread:localData.querySelector('.game-line__home-line button[data-wager-type="to"]').getAttribute('data-spread'),
+                                odd:localData.querySelector('.game-line__home-line button[data-wager-type="to"]').getAttribute('data-odds')
+                            }
+                        };
+                        console.log(visitor);
+                        datas.push({
+                            start:metaDatas.querySelector('meta[itemprop="startDate"]').getAttribute('content'),
+                            end:metaDatas.querySelector('meta[itemprop="endDate"]').getAttribute('content'),
+                            name:metaDatas.querySelector('meta[itemprop="performer"]').getAttribute('content'),
+                            visitor:visitor,
+                            home:home,
+                            id:id,
+                            url:'https://www.mybookie.ag/sportsbook/nba/?prop='+id
+                        })
+                        
+                    } catch (error) {
+                        
+                    }
                 });
 
                 return datas;
 
             });
-            console.log(urls);
+            return urls;
         }
         catch (error) {
             console.log(error);
@@ -135,7 +137,10 @@ class PuppeteerService {
 const puppeteerService = new PuppeteerService();
 puppeteerService.initiate(true)
     .then((el) => {
-        fs.writeFile(__dirname+ '/myjsonfile.json', JSON.stringify(el), 'utf8', ()=>{});
+        fs.writeFile(__dirname+ '/myjsonfile.json', JSON.stringify(el), 'utf8', (err) => {
+            if (err) {  console.error(err);  return; };
+            console.log("File has been created");
+        });
 
-        console.log(el);
+        console.log(el[0]);
     });
